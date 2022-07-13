f4f6a23713e9656955dc90fb25744e2c7afa70ef
elvyn <45512739+leElvyn@users.noreply.github.com>
2022-07-13 10:53:33 +0200

    Changed the way grid size is calculated

--- ttdashboard/ballance.py
+++ ttdashboard/ballance.py
@@ -3,5 +3,5 @@ import math
 """this file contain every ballance variables and method for the game."""
 
 def get_grid_size(number_of_players):
-    number_of_players *= 8
-    return (math.ceil(math.sqrt(number_of_players)) + 7, math.ceil(math.sqrt(number_of_players)) + 7)
+    number_of_players *= 24 #the idea is that every player takes about 24 squares of space
+    return (math.ceil(math.sqrt(number_of_players)), math.ceil(math.sqrt(number_of_players)))
